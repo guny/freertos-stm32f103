6784b21c9417b4628ee1945904c09f6d8c9e012c
guny <g_zhengyu@163.com>
2018-10-23 13:37:57 +0800

    guny
    remove *obj

--- keilv5/Objects/cjson.d
+++
@@ -1,9 +0,0 @@
-.\objects\cjson.o: ..\src\cJSON\cJSON.c
-.\objects\cjson.o: C:\Keil\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\cjson.o: C:\Keil\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\cjson.o: C:\Keil\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
-.\objects\cjson.o: C:\Keil\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\objects\cjson.o: C:\Keil\Keil_v5\ARM\ARMCC\Bin\..\include\limits.h
-.\objects\cjson.o: C:\Keil\Keil_v5\ARM\ARMCC\Bin\..\include\ctype.h
-.\objects\cjson.o: ..\src\.\cJSON\cJSON.h
-.\objects\cjson.o: C:\Keil\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
